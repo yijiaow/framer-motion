c48159cf5dd2458d1508e26976df8515189de9fc
Yijiao Wang <ttwyj250@gmail.com>
2021-10-01 14:57:27 -0700

    Framer Motion: flip, bad suspension animation

--- framer-motion/src/App.tsx
+++ framer-motion/src/App.tsx
@@ -2,17 +2,35 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Modal from './components/Modal';
 
+enum ModalType {
+  DROP_IN = 'dropIn',
+  FLIP = 'flip',
+  BAD_SUSPENSION = 'badSuspension',
+}
+
 interface AppProps {}
 
 const App: React.FC<AppProps> = ({}) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [animationType, setAnimationType] = useState<ModalType>(
+    ModalType.DROP_IN
+  );
+
+  const handleAnimationTypeChange: React.ChangeEventHandler<HTMLSelectElement> =
+    (e) => setAnimationType(e.target.value as ModalType);
 
   const open = () => setModalOpen(true);
 
   const close = () => setModalOpen(false);
 
   return (
-    <div>
+    <main>
+      <h2>Modal Animation Type</h2>
+      <motion.select onChange={handleAnimationTypeChange} className="dropdown">
+        <option value="dropIn">Drop In</option>
+        <option value="flip">Flip</option>
+        <option value="badSuspension">Bad Suspension</option>
+      </motion.select>
       <motion.button
         onClick={() => (modalOpen ? close() : open())}
         className="trigger-btn"
@@ -27,7 +45,11 @@ const App: React.FC<AppProps> = ({}) => {
         onExitComplete={() => null} // fires when all exiting nodes completed animating out
       >
         {modalOpen && (
-          <Modal modalOpen={modalOpen} handleClose={close}>
+          <Modal
+            modalOpen={modalOpen}
+            handleClose={close}
+            animationType={animationType}
+          >
             <p>
               Ea ad cupidatat duis proident. Duis cupidatat officia non
               voluptate irure eiusmod enim Lorem pariatur eu elit occaecat.
@@ -38,7 +60,7 @@ const App: React.FC<AppProps> = ({}) => {
           </Modal>
         )}
       </AnimatePresence>
-    </div>
+    </main>
   );
 };
 
--- framer-motion/src/components/Modal.tsx
+++ framer-motion/src/components/Modal.tsx
@@ -5,6 +5,7 @@ interface ModalProps {
   children: React.ReactElement;
   modalOpen: boolean;
   handleClose: () => void;
+  animationType: 'dropIn' | 'flip' | 'badSuspension';
 }
 
 const dropIn = {
@@ -28,13 +29,76 @@ const dropIn = {
   },
 };
 
-const Modal: React.FC<ModalProps> = ({ children, modalOpen, handleClose }) => {
+const flip = {
+  hidden: {
+    transform: 'scale(0) rotateX(-360deg)',
+    opacity: 0,
+    transition: {
+      delay: 0.5,
+    },
+  },
+  visible: {
+    transform: 'scale(1) rotateX(0deg)',
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: {
+    transform: 'scale(0) rotateX(360deg)',
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const badSuspension = {
+  hidden: {
+    y: '-100vh',
+    opacity: 0,
+    transform: 'scale(0) rotate(-360deg)',
+  },
+  visible: {
+    y: '-10vh',
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+      type: 'spring',
+      damping: 15,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: '-100vh',
+    opacity: 0,
+  },
+};
+
+const Modal: React.FC<ModalProps> = ({
+  children,
+  modalOpen,
+  handleClose,
+  animationType,
+}) => {
+  let variant;
+  switch (animationType) {
+    case 'dropIn':
+      variant = dropIn;
+      break;
+    case 'flip':
+      variant = flip;
+      break;
+    case 'badSuspension':
+      variant = badSuspension;
+      break;
+  }
   return (
     <Backdrop onClick={handleClose}>
       <motion.div
         onClick={(e) => e.stopPropagation()}
         className="modal orange-gradient"
-        variants={dropIn}
+        variants={variant}
         initial="hidden"
         animate="visible"
         exit="exit"
